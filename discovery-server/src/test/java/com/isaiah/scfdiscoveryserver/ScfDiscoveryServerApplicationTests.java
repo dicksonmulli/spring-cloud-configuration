27e7deb0521eef261a7b42b23aafc77b3b5c5bc2
Isaiah <dickson.isaiah6@gmail.com>
2019-12-09 15:57:59 +0300

    configuration done

---
+++ discovery-server/src/test/java/com/isaiah/scfdiscoveryserver/ScfDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaiah.scfdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScfDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
